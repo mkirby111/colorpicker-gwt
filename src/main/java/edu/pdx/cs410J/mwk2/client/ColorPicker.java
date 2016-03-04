2c0db57ca9c159c72616ad67a21850920dbed3b6
Mark Kirby <mkirby111@gmail.com>
2016-03-04 11:35:55 -0800

    No trailing whitespace here

--- src/main/java/edu/pdx/cs410J/mwk2/client/ColorPicker.java
+++ src/main/java/edu/pdx/cs410J/mwk2/client/ColorPicker.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
  * A GWT remote service that returns a dummy Phone Bill
+ *
  */
 @RemoteServiceRelativePath("ping")
 public interface ColorPicker extends RemoteService {
